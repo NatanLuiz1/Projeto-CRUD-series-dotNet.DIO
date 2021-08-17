d44aec4d62aadb94de15b48aa5765fc886839d91
Natan Luiz <nluizlla@gmail.com>
2021-08-17 03:28:57 -0300

    First Commit

---
+++ CRUD-cadastroSeries/CRUD-cadastroSeries/Classes/SerieRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUD_cadastroSeries.Interfaces;
+
+namespace CRUD_cadastroSeries.Classes
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie idObjeto)
+        {
+            listaSerie[id] = idObjeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie idObjeto)
+        {
+            listaSerie.Add(idObjeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
